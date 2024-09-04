3d4596368070ff09eb5de5e0cc26ae56f11b7f8c
Toshin Prince <toshinprince@gmail.com>
2024-09-04 16:53:21 +0530

    testRevertIfTokenLengthDoesntMatchPriceFeeds

--- test/DSCEngineTest.t.sol
+++ test/DSCEngineTest.t.sol
@@ -15,6 +15,7 @@ contract DSCEngineTest is Test {
     DSCEngine dsce;
     HelperConfig config;
     address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
     address weth;
 
     address public USER = makeAddr("user");
@@ -24,13 +25,27 @@ contract DSCEngineTest is Test {
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
-        (ethUsdPriceFeed,, weth,,) = config.activeNetworkConfig();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = config.activeNetworkConfig();
         ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
     }
 
-    //////////////////////
-    //// Price Test     //
-    //////////////////////
+    ///////////////////////////
+    /// Constructor Test     //
+    ///////////////////////////
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function testRevertIfTokenLengthDoesntMatchPriceFeeds() public {
+        tokenAddresses.push(weth);
+        priceFeedAddresses.push(ethUsdPriceFeed);
+        priceFeedAddresses.push(btcUsdPriceFeed);
+        vm.expectRevert(DSCEngine.DCSEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength.selector);
+        new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+    }
+
+    ////////////////////
+    /// Price Test    //
+    ////////////////////
 
     function testGetUsdValue() public {
         uint256 ethAmount = 15e18;
